a4871c133b857725d112929e8b515e0e20cbc4d5
wisnu <wisnu.septiaji@gmail.com>
2019-11-07 20:03:58 +0700

    completing the client

--- burrow.go
+++ burrow.go
@@ -28,12 +28,29 @@ func WithHTTPClient(httpClient *http.Client) Option {
 
 // Client is the interface that should be implemented by client struct
 type Client interface {
+	// Ping is used to check burrow healthiness
 	Ping(ctx context.Context) error
-	Clusters(ctx context.Context) ([]string, error)
-	Consumers(ctx context.Context, cluster string) ([]string, error)
-	Topics(ctx context.Context, cluster string) ([]string, error)
 
+	// Clusters will return either list of clusters name or error
+	Clusters(ctx context.Context) (Clusters, error)
+
+	// Consumers will return either list of consumers name or error
+	Consumers(ctx context.Context, cluster string) (Consumers, error)
+
+	// Topics will return either list of topics name or error
+	Topics(ctx context.Context, cluster string) (Topics, error)
+
+	// Cluster will return either cluster detail or error
 	Cluster(ctx context.Context, name string) (Cluster, error)
+
+	// Consumer will return either consumer detail or error
+	Consumer(ctx context.Context, cluster, name string) (Consumer, error)
+
+	// Topic will return either topic detail or error
+	Topic(ctx context.Context, cluster, name string) (Topic, error)
+
+	// DeleteConsumer will delete consumer through burrow and return error if fail
+	DeleteConsumer(ctx context.Context, cluster, consumer string) error
 }
 
 type baseResponse struct {
@@ -44,6 +61,21 @@ type baseResponse struct {
 		Host string `json:"host"`
 	} `json:"request"`
 }
+
+// Clusters is the type for cluster list
+type Clusters []string
+
+// Consumers is the type for consumer list
+type Consumers []string
+
+// Topics is the type for topic list
+type Topics []string
+
+// Topic is the type for topic detail
+type Topic []int
+
+// Consumer is the type for consumer detail
+type Consumer map[string][]ConsumerTopicDetail
 
 // Cluster is the structure for cluster detail
 type Cluster struct {
@@ -56,4 +88,15 @@ type Cluster struct {
 	OffsetRefresh int      `json:"offset-refresh"`
 	Servers       []string `json:"servers"`
 	TopicRefresh  int      `json:"topic-refresh"`
+}
+
+// ConsumerTopicDetail is the structure for consumer topic detail
+type ConsumerTopicDetail struct {
+	CurrentLag int `json:"current-lag"`
+	Offsets    []struct {
+		Lag       int `json:"lag"`
+		Offset    int `json:"offset"`
+		Timestamp int `json:"timestamp"`
+	} `json:"offsets"`
+	Owner string `json:"owner"`
 }
--- client.go
+++ client.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 )
 
+var _ Client = (*client)(nil)
+
 // NewClient creates new client pointer returned as Client interface
 func NewClient(address string, opts ...Option) Client {
 	c := new(config)
@@ -59,16 +61,17 @@ func (c *client) Ping(ctx context.Context) error {
 	return nil
 }
 
-func (c *client) Clusters(ctx context.Context) ([]string, error) {
+func (c *client) Clusters(ctx context.Context) (Clusters, error) {
+	var resp struct {
+		baseResponse
+		Clusters Clusters `json:"clusters"`
+	}
+
 	b, err := c.sendGet(ctx, fmt.Sprintf("%s%s", c.address, basePath))
 	if err != nil {
 		return nil, err
 	}
 
-	var resp struct {
-		baseResponse
-		Clusters []string `json:"clusters"`
-	}
 	if err := json.Unmarshal(b, &resp); err != nil {
 		return nil, err
 	}
@@ -80,16 +83,17 @@ func (c *client) Clusters(ctx context.Context) ([]string, error) {
 	return resp.Clusters, nil
 }
 
-func (c *client) Consumers(ctx context.Context, cluster string) ([]string, error) {
+func (c *client) Consumers(ctx context.Context, cluster string) (Consumers, error) {
+	var resp struct {
+		baseResponse
+		Consumers Consumers `json:"consumers"`
+	}
+
 	b, err := c.sendGet(ctx, fmt.Sprintf("%s%s/%s/consumer", c.address, basePath, cluster))
 	if err != nil {
 		return nil, err
 	}
 
-	var resp struct {
-		baseResponse
-		Consumers []string `json:"consumers"`
-	}
 	if err := json.Unmarshal(b, &resp); err != nil {
 		return nil, err
 	}
@@ -101,16 +105,17 @@ func (c *client) Consumers(ctx context.Context, cluster string) ([]string, error
 	return resp.Consumers, nil
 }
 
-func (c *client) Topics(ctx context.Context, cluster string) ([]string, error) {
+func (c *client) Topics(ctx context.Context, cluster string) (Topics, error) {
+	var resp struct {
+		baseResponse
+		Topics Topics `json:"topics"`
+	}
+
 	b, err := c.sendGet(ctx, fmt.Sprintf("%s%s/%s/topic", c.address, basePath, cluster))
 	if err != nil {
 		return nil, err
 	}
 
-	var resp struct {
-		baseResponse
-		Topics []string `json:"topics"`
-	}
 	if err := json.Unmarshal(b, &resp); err != nil {
 		return nil, err
 	}
@@ -123,18 +128,96 @@ func (c *client) Topics(ctx context.Context, cluster string) ([]string, error) {
 }
 
 func (c *client) Cluster(ctx context.Context, name string) (Cluster, error) {
-	b, err := c.sendGet(ctx, fmt.Sprintf("%s%s/%s", c.address, basePath, name))
-	if err != nil {
-		return Cluster{}, err
-	}
-
 	var resp struct {
 		baseResponse
 		Module Cluster `json:"module"`
 	}
+
+	b, err := c.sendGet(ctx, fmt.Sprintf("%s%s/%s", c.address, basePath, name))
+	if err != nil {
+		return resp.Module, err
+	}
+
 	if err := json.Unmarshal(b, &resp); err != nil {
-		return Cluster{}, nil
+		return resp.Module, err
+	}
+
+	if resp.Error {
+		return resp.Module, errors.New(resp.Message)
 	}
 
 	return resp.Module, nil
+}
+
+func (c *client) Consumer(ctx context.Context, cluster, name string) (Consumer, error) {
+	var resp struct {
+		baseResponse
+		Topics Consumer `json:"topics"`
+	}
+
+	b, err := c.sendGet(ctx, fmt.Sprintf("%s%s/%s/consumer/%s", c.address, basePath, cluster, name))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(b, &resp); err != nil {
+		return nil, err
+	}
+
+	if resp.Error {
+		return nil, errors.New(resp.Message)
+	}
+
+	return resp.Topics, nil
+}
+
+func (c *client) Topic(ctx context.Context, cluster, name string) (Topic, error) {
+	var resp struct {
+		baseResponse
+		Offsets Topic `json:"offsets"`
+	}
+
+	b, err := c.sendGet(ctx, fmt.Sprintf("%s%s/%s/topic/%s", c.address, basePath, cluster, name))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(b, &resp); err != nil {
+		return nil, err
+	}
+
+	if resp.Error {
+		return nil, errors.New(resp.Message)
+	}
+
+	return resp.Offsets, nil
+}
+
+// nolint: errcheck
+func (c *client) DeleteConsumer(ctx context.Context, cluster, consumer string) error {
+	path := fmt.Sprintf("%s%s/%s/topic/%s", c.address, basePath, cluster, consumer)
+	req, err := http.NewRequest(http.MethodDelete, path, nil)
+	if err != nil {
+		return err
+	}
+	req = req.WithContext(ctx)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	var r struct {
+		baseResponse
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+		return err
+	}
+
+	if r.Error {
+		return errors.New(r.Message)
+	}
+
+	return nil
 }
